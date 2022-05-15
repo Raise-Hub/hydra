a7de757172995cadd347d9e16865b6111fd373d4
BETHA\mario.lo <mario.lo@betha.com.br>
2022-05-15 15:49:20 -0300

    hydra - alteração nome da pasta

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hydra/modules/home_page/home_page.dart';
+import 'package:hydra/modules/home/home_page.dart';
 
 import 'modules/login/login_page.dart';
 
